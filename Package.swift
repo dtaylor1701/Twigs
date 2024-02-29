6ea08254ff0f2b3d58dfed4e7f1430026c64fcb7
David Taylor <dktaylor1701@gmail.com>
2024-02-28 23:49:45 -0500

    Updated argument parser

--- Package.swift
+++ Package.swift
@@ -4,27 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "Twigs",
-    platforms: [.macOS(.v12)],
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Twigs",
-            targets: ["Twigs"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "Twigs",
-            dependencies: [
-                .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ]),
-        .testTarget(
-            name: "TwigsTests",
-            dependencies: ["Twigs"]),
-    ]
+  name: "Twigs",
+  platforms: [.macOS(.v13)],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "Twigs",
+      targets: ["Twigs"])
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "Twigs",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ]),
+    .testTarget(
+      name: "TwigsTests",
+      dependencies: ["Twigs"]),
+  ]
 )
